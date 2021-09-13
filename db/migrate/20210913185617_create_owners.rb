1374a2fb905347c6ba0f56f2b6ffbe888307441e
Selina Serphin <sserphin@optoro.com>
2021-09-13 17:29:27 -0400

    Created the Owner model db table.

---
+++ db/migrate/20210913185617_create_owners.rb
@@ -0,0 +1,11 @@
+class CreateOwners < ActiveRecord::Migration[6.0]
+  def change
+    create_table :owners do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :contact_number
+
+      t.timestamps
+    end
+  end
+end

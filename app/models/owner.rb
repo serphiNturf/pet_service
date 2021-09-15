7a011005e2a1c1c1285b2beb13a5bfe9105311d2
Selina Serphin <sserphin@optoro.com>
2021-09-14 20:36:57 -0400

    Added validation to fields on the Owner model and created a full_name method in the owner model file.

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,9 @@
 class Owner < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :contact_number, presence: true, uniqueness: true
+
+  def full_name
+    first_name + " " + last_name
+  end
 end

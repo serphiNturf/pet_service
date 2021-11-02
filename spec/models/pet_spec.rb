f10c4f70e25482f4a29529abcf9002c3a32f0bcb
Selina Serphin <sserphin@optoro.com>
2021-11-02 17:33:20 -0400

    Cleaned up more comments and my syntax in some of the it blocks

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -3,21 +3,20 @@ require 'rails_helper'
 RSpec.describe Pet, type: :model do
   let (:pet) {FactoryBot.create(:pet)}
 
-  it "Pet should have a name" do
+  it "should have a name" do
     expect(pet.name).to eq("Lulu")
   end
 
-  it "Pet should have a gender" do
+  it "should have a gender" do
     expect(pet.gender).to eq("Female")
   end
 
-  it "Pet should have a species" do
+  it "should have a species" do
     expect(pet.species).to eq("Cat")
   end
 
   it "should not have an owner yet" do
     expect(pet.owners).to be_empty
-    #expect(pet.owners).to eq([])
   end
 
   context "when a pet has an owner" do
@@ -26,7 +25,7 @@ RSpec.describe Pet, type: :model do
       OwnersPets.create(pet_id: pet.id, owner_id: owner.id)
     end
     it "should have an owner" do
-      expect(pet.owners).to be_empty
+      expect(pet.owners).to eq([owner])
     end
   end
 

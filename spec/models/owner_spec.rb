4281cb787113ad7e5e6da290cd29287124fd2957
Selina Serphin <sserphin@optoro.com>
2021-11-15 15:37:18 -0500

    Added spec for full_name method on Owner model

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -3,16 +3,21 @@ require 'rails_helper'
 RSpec.describe Owner, type: :model do
   let (:owner) {FactoryBot.create(:owner)}
 
-  it "Owner should have a first name" do
+  it "should have a first name" do
     expect(owner.first_name).to eq("James")
   end
 
-  it "Owner should have a last name" do
+  it "should have a last name" do
     expect(owner.last_name).to eq("Carter")
   end
 
-  it "Owner should have a phone number" do
+  it "should have a phone number" do
     expect(owner.contact_number).to eq("2065557878")
   end
 
+  context "#full_name" do
+    it "should should have a first name and last name" do
+      expect(owner.full_name).to eq("James Carter")
+    end
+  end
 end

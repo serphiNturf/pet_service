9212a144d4f75c2e8bdd54a427fec3bc26f98905
Selina Serphin <sserphin@optoro.com>
2021-11-30 15:13:34 -0500

    Added the owners_controller and fixed rubocop violations

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This is the Application Record Base class
 
 class ApplicationRecord < ActiveRecord::Base
--- spec/models/owners_pets_spec.rb
+++ spec/models/owners_pets_spec.rb
@@ -3,9 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe OwnersPets, type: :model do
-  let(:owners_pets) {FactoryBot.create(:owners_pets)}
-  let(:owner) {owners_pets.owner}
-  let(:pet) {owners_pets.pet}
+  let(:owners_pets) { FactoryBot.create(:owners_pets) }
+  let(:owner) { owners_pets.owner }
+  let(:pet) { owners_pets.pet }
 
   it 'should have an owner_id' do
     expect(owners_pets.owner_id).to eq(owner.id)
